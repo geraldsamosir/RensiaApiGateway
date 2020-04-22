7ded4d265eac1c67495359c8193ea5b35d8bea84
geraldsamosir <gerald.unforgiven@gmail.com>
2020-04-22 09:21:42 +0700

    add infrastucture

---
+++ Gateway/Njs-Midleware/roles-midleware.js
@@ -0,0 +1,13 @@
+/**
+ * @author Gerald Halomoan Samosir
+ * @since 22/04/2020
+ */
+
+/**
+ * 
+ * @param {object} r nginx context will handle on this midleware
+ * @returns {object} will return json response 
+ */
+function checkAllowAccessRouter(r){
+
+}
---
+++ Infrastructure/Readme.md
@@ -0,0 +1,5 @@
+## Infrastructure 
+
+this is the infra tools thas your services
+need instaling in  here
+like database or cache 
